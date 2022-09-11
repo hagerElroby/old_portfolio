2cd842992be9790c4d80f38574e39919b253504e
hagerElroby <hagerelroby.az2020@gmail.com>
2022-09-11 20:45:57 +0200

    old portfolio

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+export const products = [
+      {
+        id: 1,
+        img: require('./img/2.jpg'),
+        link: "https://hagerelroby.github.io/nokoSocial-/",
+      },
+      {
+        id: 2,
+        img: require('./img/1.jpg'),
+        link: "https://hagerelroby.github.io/Agency_Template-/",
+      },
+      {
+        id: 3,
+        img: require('./img/3.jpg'),
+        link: "https://recipe-web-app.vercel.app/",
+      },
+      {
+        id: 4,
+        img:  require('./img/4.jpg'),
+        link: "https://react18-gym-responsive-app.vercel.app/",
+      },
+      {
+        id: 5,
+        img: require('./img/5.jpg'),
+        link: "https://hagerelroby.github.io/Html-Css-Templete-One/",
+      },
+      {
+        id: 6,
+        img: require('./img/6.jpg'),
+        link: "https://to-do-list-app-pi-seven.vercel.app/",
+      },
+    ];
+    
+    
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8EXrWr7Yv9BDDlbOavcBZXh2dFQDwB_U",
+  authDomain: "portfolio-75d74.firebaseapp.com",
+  projectId: "portfolio-75d74",
+  storageBucket: "portfolio-75d74.appspot.com",
+  messagingSenderId: "831515813125",
+  appId: "1:831515813125:web:99e026da3db12370dc436c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
